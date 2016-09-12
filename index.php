d5b48bb94a1b4e94518498085047c9c38320acad
Angel Flores <aaflores93@gmail.com>
2016-09-12 12:51:15 -0500

    initialized

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php ?>
+<html>
+<head>
+	<title>Tweek</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/custom.css">
+
+	
+</head>
+<body class="pic">
+<div class ="container">
+	 <header id="navigation" class="navbar-fixed-top tendered">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+                    <button type="button" class="navbar-toggle" style ="color: white"data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- /responsive nav button -->
+
+                    <!-- logo --> 
+                    <div class="navbar-brand">
+                        <a href="#home">
+                            <img  alt="Tweek" src="img/logo.png" >
+                        </a>
+                    </div>
+                    <!-- /logo -->
+
+                    </div>
+
+                    <!-- main nav -->
+                    <nav class="collapse navigation navbar-collapse navbar-right" role="navigation">
+                        <ul id="nav" class="nav navbar-nav">
+                            <li class="current"><a href="#home">About us</a></li>
+                            <!--li><a href="#service">Service</a></li-->
+                            
+                            <!--li><a href="#contact">Contact</a></li-->
+                        </ul>
+                    </nav>
+                    <!-- /main nav -->
+                </div>
+
+            </div>
+        
+        </header>
+        
+        <br><br><br><br><br><br>
+
+	<div class="container blacked">
+		<div class ="row text-center">
+			<h1>A more personalized magazine</h1>
+		</div>
+		<div class="row ">
+			
+			<div class="col-md-6 vertical-line">
+				<h4>Register</h4>
+				<form class="form">
+					<div class="form-group">
+						<label for="name">Name*</label>
+						<input type="name" id="name" name="name" class ="form-control" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="email">Email*</label>
+						<input type="email" id="email" name="email" class ="form-control" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="password">Password*</label>
+						<input type="password" id="password" name="password" class ="form-control" placeholder="Password">
+					</div>
+					<div class="form-group">
+						<label for="passwordRepeat">Repeat password*</label>
+						<input type="password" id="passwordRepeat" name="passwordRepeat" class ="form-control" placeholder="Repeat password">
+					</div>
+					<div class="form-group">
+						<input type="submit" class ="btn btn-primary form-control" value="Register">
+					</div>
+				</form>
+			</div>
+		
+			<div class="col-md-6 ">
+			<h4>Login</h4>
+				<form class="form">
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" id="email" name="email" class ="form-control" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="password">Password</label>
+						<input type="password" id="password" name="password" class ="form-control" placeholder="Password">
+					</div>
+					<div class="form-group">
+						<input type="submit" class ="btn btn-success form-control" value="Login">
+					</div>
+				</form>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	
+ 
+  
+    
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+	
+<body>
+
+</html>
